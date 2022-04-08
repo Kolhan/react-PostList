4286b04f6c81a8cfabc70d84be07447fb718fa78
Kolhan <kolhan@mail.ru>
2022-04-08 15:42:22 +0500

    правка 4

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),//путь куда будет собираться наш проект
 		filename: "main.js", // имя нашего бандла
-		// publicPath: "./dist/"
+		publicPath: path.resolve(__dirname,"")
 	},
 	devServer: {
 		historyApiFallback: true, //включает роутинг
